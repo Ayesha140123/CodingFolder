f8f3f467ee24ce752a7232dbcca4d47f5d6f6aac
Ayesha140123 <ayesha.rizwan12345@gmail.com>
2025-01-09 16:41:47 +0500

    Create Python_1.py
    This is a Python code that assigns a grade to the user when they enter a value for a grade. If the value is greater than or equal to 90, it will give an "A" grade. If the value is greater than or equal to 80, it will give a "B" grade, and if the value is greater than or equal to 70, it will give a "C" grade.

---
+++ BBT243009_code/Python_1.py
@@ -0,0 +1,7 @@
+grade=int(input("enter value for grade="))
+if grade>=90:
+    print("Your grade is= A")
+elif grade>=80:
+    print("Your grade is= B")
+elif grade>=70:
+    print("Your grade is= C")
